7df1698efcc9f1f851d01af5b4517b4820f757c8
niall <N00242247@iadt.ie>
2026-01-20 12:20:37 +0000

    basic array exercies

--- sketch.js
+++ sketch.js
@@ -1,3 +1,29 @@
+let names = ["Alice", "Bob", "Charlie", "David"];
+console.log(names[2]);
+
+names[1] = "Bobby"; 
+console.log(names[1]);
+
+console.log(names.length);
+
+names.push("David");
+names.push("Eve");
+names.push("Frank");
+console.log(names);
+
+names.pop();
+console.log(names);
+
+names.shift();
+console.log(names);
+
+names.unshift("Zara");
+console.log(names);
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
 function setup() {
 	createCanvas(500, 500);
 	background(55);
